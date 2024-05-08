398794bde7b41d08fdfe091fef699d04b46f237a
kumaara ganapathi n <kumaaraganapathin.221cs133@nitk.edu.in>
2024-05-08 11:10:18 +0530

    added basic infrastructure

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::{fs::File, io::Read};
 use chip8::Chip8;
 mod ram;
 mod chip8;
+mod cpu;
+
 
 fn main(){
-    let mut file= File::open("data/INVADERS").unwrap();
+    let mut file = File::open("data/BLITZ").unwrap();
+
     let mut data=Vec::<u8>::new();
     file.read_to_end(&mut data);
 
     let mut chip8=Chip8::new();
     chip8.load_rom(&data);
     
+    loop {
+        chip8.run_instruction();
+    }
 
 }
